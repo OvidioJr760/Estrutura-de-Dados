63628e6215a187efde8fb3767c3c7fcb18f7268d
ovidiojr <ovidioavj@gmail.com>
2021-05-03 16:08:58 -0300

    comitando primeiro post

---
+++ testeencadeada.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "pilha.h"
+#include "pilha_encadeada.c"
+#include "time.h"
+
+Pilha* p = pilha_criar();
+
+void pilha_testeempilhar(){
+
+  int i;
+
+  int tamVetor = 10000000;
+  for ( i = 0; i < tamVetor; i++ ) {
+    pilha_empilhar(p,i);
+  }
+}
+
+void pilha_testedesempilhar(){
+
+  int i;
+  TipoElemento elemaux;
+
+  int tamVetor = 10000000;
+  for ( i = 0; i < tamVetor; i++ ) {
+    pilha_desempilhar(p,&elemaux);
+  }
+}
+
+
+int main(){
+	clock_t tempo;
+	
+	tempo = clock();
+	
+	pilha_testeempilhar();
+	printf("Tempo Empilhar: %f",(clock() - tempo)/(double)CLOCKS_PER_SEC);
+	
+    tempo = clock();
+
+	pilha_testedesempilhar();
+	printf("\nTempo Desempilhar: %f",(clock() - tempo)/(double)CLOCKS_PER_SEC);
+	
+	//pilha_imprimir(p);
+	pilha_destruir(p);
+	return 0;
+}
+
